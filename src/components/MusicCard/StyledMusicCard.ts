fc12ac8fc17d5ef6e21fc389c2866ffe91643efd
Thales <thsm271@gmail.com>
2021-10-13 21:40:12 -0300

    created songsScreen

--- src/components/MusicCard/StyledMusicCard.ts
+++ src/components/MusicCard/StyledMusicCard.ts
@@ -13,7 +13,36 @@ export const StyledMusicDiv = styled.div`
   color: ${Colors.weakWhite};
   width: 800px;
   height: 300px;
-  margin-left: 10px;
-  margin-right: 10px;
-  margin-top: 10px;
+  margin: 0px 10px 20px 10px;
+`;
+
+export const StyledImageTextDiv = styled.div`
+  margin: 10px 0 10px 10px;
+`;
+
+export const StyledFeaturesDiv = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 10px 10px 10px 10px;
+  flex-direction: column;
+`;
+
+export const StyledHeartIconDiv = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const StyledTemperatureImageDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: -20px;
+`;
+
+export const StyledMusicLinksDiv = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
 `;
